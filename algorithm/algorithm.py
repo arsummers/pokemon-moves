d0644e8b642d1ca4f72acfbc5446053afa3a2d7a
arsummers <aliyasummers1@gmail.com>
2020-08-20 17:17:06 -0700

    modifiers

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -1,5 +1,13 @@
-def modifier():
-    pass
+def modifier(targets, weather, badge, critical, random, stab, move_type, burn, other):
+     if targets > 1:
+         target_mod = 0.75
+    else:
+        target_mod = 1
+
+    if weather == 'rain' and move_type == 'water' or weather == 'harsh sunlight' and move_type == 'fire':
+        weather_mod = 1.5
+
+    
 
 def damage(level, power, attack, defense):
 
@@ -8,3 +16,5 @@ def damage(level, power, attack, defense):
     attack_def_mod = attack / defense
 
     dmg = (((level_mod * power * attack_def_mod) / 50) + 2) * modifier
+
+    return f'overall approximate damage: {dmg}'
---
+++ algorithm/test_algorithm.py
@@ -0,0 +1,8 @@
+from algorithm import modifier, damage
+
+def test_exists():
+    assert modifier
+    assert damage
+
+def test_one():
+    pass
