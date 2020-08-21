2cfb523a6546efd6b7e6984f6b651df460647761
arsummers <aliyasummers1@gmail.com>
2020-08-20 17:56:02 -0700

    more mods

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -1,4 +1,7 @@
-def modifier(targets, weather, badge, critical, random, stab, move_type, burn, other):
+class UserPokemon(type, attack, defense, spec_attack, spec_defense, ability):
+    pass
+
+def modifier(targets, weather, random, stab, move_type, burn, other):
      if targets > 1:
          target_mod = 0.75
     else:
@@ -6,7 +9,21 @@ def modifier(targets, weather, badge, critical, random, stab, move_type, burn, o
 
     if weather == 'rain' and move_type == 'water' or weather == 'harsh sunlight' and move_type == 'fire':
         weather_mod = 1.5
+    elif weather == 'rain' and move_type == 'fire' or weather == 'harsh sunlight' and move_type == 'water':
+        weather_mod = 0.5
+    else:
+        weather_mod = 1
 
+    # this syntax is wrong
+    if move_type in UserPokemon_type and UserPokemon_ability == 'adaptability':
+        stab = 2.0
+    if move_type in UserPokemon_type:
+        stab = 1.5
+    else:
+        stab = 1
+
+    
+    
     
 
 def damage(level, power, attack, defense):
