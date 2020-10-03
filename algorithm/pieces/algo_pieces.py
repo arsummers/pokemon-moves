af69c0677a96809406245c7da6352e855c9abe6f
arsummers <aliyasummers1@gmail.com>
2020-10-02 17:29:43 -0700

    kind of a fixture constructed

--- algorithm/pieces/algo_pieces.py
+++ algorithm/pieces/algo_pieces.py
@@ -6,3 +6,25 @@ def target_num(targets):
 
         return target_mod
         
+def weather_modifier(weather, move_type):
+        if weather == 'rain' and move_type == 'water' or weather == 'harsh sunlight' and move_type == 'fire':
+            weather_mod = 1.5
+        elif weather == 'rain' and move_type == 'fire' or weather == 'harsh sunlight' and move_type == 'water':
+            weather_mod = 0.5
+        else:
+            weather_mod = 1
+        return weather_mod
+
+
+class UserPokemon:
+    # probs gonna have to pass some of these in but will need to think about it.
+    def __init__(self, species, species_type, attack, defense, spec_attack, spec_defense, ability, move_type, level):
+        self.species = species #str
+        self.species_type = user_type #lst
+        self.attack = attack
+        self.defense = defense
+        self.spec_attack = spec_attack
+        self.spec_defense = spec_defense
+        self.ability = ability
+        self.move_type = move_type #move type it is using
+        self.level = level
--- algorithm/pieces/test_algo_pieces.py
+++ algorithm/pieces/test_algo_pieces.py
@@ -1,8 +1,32 @@
 import pytest
 from algo_pieces import *
 
-def test_targets():
+def test_2_targets():
     targets = 2
     expected = 0.75
     actual = target_num(targets)
     assert expected == actual
+
+def test_1_target():
+    targets = 1
+    expected = 1
+    actual = target_num(targets)
+    assert expected == actual
+
+@pytest.fixture
+def user_pokemon_one():
+    u_poke = UserPokemon()
+    u_poke.species = 'bulbasaur'
+    u_poke.species_type = ['', '']
+    u_poke.attack = 1
+    u_poke.defense = 1
+    u_poke.spec_attack = 1
+    u_poke.spec_defense = 1
+    u_poke.ability = 'no'
+    u_poke.move_type = 'grass'
+    u_poke.level = 5
+
+
+    
+
+    pass
