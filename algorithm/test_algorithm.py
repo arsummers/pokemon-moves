2f10fb02cb3211e5e4e09623e57897bb21dd31a5
arsummers <aliyasummers1@gmail.com>
2021-07-22 17:26:55 -0700

    test planning

--- algorithm/test_algorithm.py
+++ algorithm/test_algorithm.py
@@ -5,3 +5,5 @@ def test_exists():
 
 def test_one():
     pass
+
+# should test for is A > B, approx damage. Tests for things that are type effective, type ineffective, spec vs physical, pokemon with same level, pokemon with level disparity, 
